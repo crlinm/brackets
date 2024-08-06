a9e6260c5b2d5cb66be91b3c6a797319c529ff9c
Alina M <crlin.m@gmail.com>
2024-08-06 14:34:44 +0500

    feat: implement algorithm for checking bracket sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+ let stack = [];
+
+ let brackets_map = new Map();
+ for (let i = 0; i < bracketsConfig.length; i++){
+  brackets_map.set(bracketsConfig[i][0], bracketsConfig[i][1]);
+ }
+ 
+ for (let i = 0; i < str.length; i++){
+    const symb = str[i];
+    console.log('brackets_map.has(symb): ', brackets_map.has(symb), symb);
+
+    if (stack.length > 0){
+      const last_br = stack[stack.length-1];
+      if (brackets_map.has(last_br) && brackets_map.get(last_br) == symb){
+        stack.pop();
+      } else {
+        stack.push(symb);}
+    } else {
+      stack.push(symb);
+    }
+  }
+
+ return stack.length == 0;
 }
